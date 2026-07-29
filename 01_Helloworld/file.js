d8b4af3332d319dd2b454a6876757cdb7759aea1
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-07-29 23:41:21 +0500

    basics handled

--- 01_Helloworld/file.js
+++ 01_Helloworld/file.js
@@ -1,26 +1,48 @@
 const fs = require('fs');
 
-fs.writeFileSync("./test.txt", "Hey There");
+const os = require('os');
+console.log(os.cpus().length);
 
-fs.writeFile("./test2.txt", "Hey There", (err)=>{});
+// fs.writeFileSync("./test.txt", "Hey There");
 
-// console.log(fs.readFileSync("./contacts.txt", "utf-8"));
-// fs.readFileSync("./contacts.txt", "utf-8", (error , result)=>{
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log(result);
-//     }
-// })
+// // fs.writeFile("./test2.txt", "Hey There", (err)=>{});
 
-fs.appendFileSync("./test.txt", " Hey There Again");
+// // // console.log(fs.readFileSync("./contacts.txt", "utf-8"));
+// // // fs.readFileSync("./contacts.txt", "utf-8", (error , result)=>{
+// // //     if(error){
+// // //         console.log(error);
+// // //     }else{
+// // //         console.log(result);
+// // //     }
+// // // })
 
-fs.cpSync("./test.txt", "./test2.txt");
+// // fs.appendFileSync("./test.txt", " Hey There Again");
 
-fs.unlinkSync("./test2.txt");
+// // fs.cpSync("./test.txt", "./test2.txt");
 
-console.log(fs.statSync("./test.txt"));
+// // fs.unlinkSync("./test2.txt");
 
-// fs.mkdirSync("my-doc");
+// // console.log(fs.statSync("./test.txt"));
 
-fs.mkdirSync("my-doc/s/t" , {recursive:true});
+// // fs.mkdirSync("my-doc");
+
+// fs.mkdirSync("my-doc/s/t" , {recursive:false});
+// the sequence in output is same
+// blocking code
+// console.log("1")
+// const result = fs.readFileSync('contacts.txt', 'utf-8');
+// console.log(result);
+// console.log("2");
+
+// we get different sequence in output
+// non-blocking code
+//1
+// undefined
+// 2
+// mehreen : +92222222
+console.log("1")
+const result = fs.readFile('contacts.txt', 'utf-8', (error , result)=>{
+    console.log(result);
+});
+console.log(result);
+console.log("2");

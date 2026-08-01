fadb21b5fc58e1d820352f23d43e24c2919a1914
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-08-01 09:15:34 +0500

    commits

--- 03_Project01/index.js
+++ 03_Project01/index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+
+const app = express();
+const PORT = 8622;
+
+const users = require('./MOCK_DATA.json');
+// routes
+
+app.get('/users', (req, res)=>{
+    return res.json(users);
+});
+
+
+
+app.listen(PORT, ()=>{console.log(`Server is running on port ${PORT}`)});

1ba8f767e131b5bf2139b42d33f6581d8d0e4e78
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-08-03 16:04:48 +0500

    commit

--- 03_Project01/controllers/user.js
+++ 03_Project01/controllers/user.js
@@ -18,9 +18,57 @@ async function handleGetUserById(req, res) {
     }
 }
 
+async function handleUpdateUserbyId(req, res) {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { lastName: "changed" }, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ status: 'error', message: 'User not found' });
+        }
+        return res.json({ status: 'success', data: updatedUser });
+    } catch (err) {
+        return res.status(500).json({ status: 'error', message: err.message });
+    }
+}
+
+async function handleDeleteUserById(req, res) {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).json({ status: 'error', message: 'User not found' });
+        }
+        return res.json({ status: 'success' });
+    } catch (err) {
+        return res.status(500).json({ status: 'error', message: err.message });
+    }
+}
+
+async function handleCreateUser(req, res) {
+    try {
+        const body = req.body;
+        if (!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
+            return res.status(400).json({ status: 'error', message: 'Missing required fields' });
+        }
+        const result = await User.create({
+            firstName: body.first_name,
+            lastName: body.last_name,
+            email: body.email,
+            jobTitle: body.job_title,
+            gender: body.gender,
+
+        });
+        console.log(result)
+        return res.status(201).json({ msg: 'User created successfully' });
+    } catch (err) {
+        return res.status(500).json({ status: 'error', message: err.message });
+    }
+}
+
 
 
 module.exports = {
     handleGetAllUsers,
-    handleGetUserById
+    handleGetUserById,
+    handleUpdateUserbyId,
+    handleDeleteUserById,
+    handleCreateUser
 }

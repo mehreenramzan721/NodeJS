78308e6cfc3f27b647da40e3d70ce0522e5e6751
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-08-03 16:29:38 +0500

    commit

--- 03_Project01/mongoose.js
+++ 03_Project01/mongoose.js
@@ -21,6 +21,6 @@ app.use(logReqRes('log.txt'));
 
 
 // router connect
-app.use('/api/user', userRouter);
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) });

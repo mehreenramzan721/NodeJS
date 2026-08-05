ea3ba25fb99989ee2fea3553ec7c03c6ee30c9ce
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-08-06 00:37:31 +0500

    commit

--- 07_BloggingApp/index.js
+++ 07_BloggingApp/index.js
@@ -1,9 +1,14 @@
 const express = require ('express');
-
+const path = require('path')
 const app = express();
+app.set('viewengine','ejs' );
+app.set('views',path.resolve("./views"));
 
 const PORT = 8000;
 
+app.get('/', (req,res)=>{
+    res.render('home')
+})
 app.listen(PORT , ()=>{
     console.log(`Server running at port : ${PORT}`)
 })

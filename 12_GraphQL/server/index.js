69b67bf11c753f4e7a5cf766c101d2bcab90cb07
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-08-26 15:34:00 +0000

    commit

--- 12_GraphQL/server/index.js
+++ 12_GraphQL/server/index.js
@@ -4,23 +4,40 @@ const { expressMiddleware } = require('@as-integrations/express5');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const { default: axios } = require("axios");
+
+
 async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
+        type User{
+        name: String!
+        }
     type Todo {
         id: ID!
         title: String!
         completed: Boolean
+        user: User
     }
     type Query {
         getTodos: [Todo]
+        getAllUsers: [User]
+        getUser(id: ID!): User
+
+
     }
 `,
         resolvers: {
+            Todo: {
+        user: async (todo) => (await axios.get(`https://dummyjson.com/users/${todo.userId}`)).data,
+      },
     Query: {
-        getTodos: () => [{ id: 1, title: "Something", completed: false }]
-    }
+        // getTodos: () => [{ id: 1, title: "Something", completed: false }]
+        getTodos: async () => (await axios.get(`https://dummyjson.com/todos`)).data.todos,
+        getAllUsers: async () => (await axios.get(`https://dummyjson.com/users`)).data.users,
+        getUser: async (_, { id }) => (await axios.get(`https://dummyjson.com/users/${id}`)).data
+}
 }
     })
 

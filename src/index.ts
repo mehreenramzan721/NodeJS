c01b047006692e6dd719833a1a1bc13ec0beefd8
MEHREEN RAMZAN <mehreenramzan721@gmail.com>
2026-08-27 06:53:22 +0000

    Fix module resolution errors, switch to @as-integrations/express5

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { ApolloServer } from "@apollo/server";
+
+import { expressMiddleware } from '@as-integrations/express5';
+async function init(){
+//PORT = 8000
+const PORT = Number(process.env.PORT) || 8000;
+
+// Create graphql server 
+const gqlserver = new ApolloServer({
+    typeDefs: ``,
+    resolvers:{}
+})
+//start the gqlserver
+await gqlserver.start();
+
+
+const app = express();
+app.use(express.json())
+app.get('/', ( req, res)=>{
+    res.json({message:"Server is up and running "})
+})
+app.use(
+    "/graphql",
+    expressMiddleware(gqlserver));
+
+app.listen (PORT , ()=> console.log(`Server is running at port ${PORT}`))
+}
+
+init();
